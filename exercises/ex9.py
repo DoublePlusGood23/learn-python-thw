f20371e11225032f02114603c35054f3b34ec34c
Ethan R. Jones <doubleplusgood23@gmail.com>
2015-02-21 15:44:58 -0500

    Multiple lined strings.

---
+++ exercises/ex9.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thi Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days: ", days
+print "Here are the months: ", months
+
+print """
+There's something going on here.
+with the three double-quotes.
+We'll be able to type as we like.
+Even 4 lines if we want, or 5 or 6.
+"""
