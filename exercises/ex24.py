885ab03cb9771eb1ce9b8de70d7e581d1aed29d6
Ethan R. Jones <doubleplusgood23@gmail.com>
2015-02-22 21:27:44 -0500

    Yay functions!

--- exercises/ex24.py
+++ exercises/ex24.py
@@ -21,7 +21,7 @@ def secret_formula(started):
     jelly_beans = started * 500
     jars = jelly_beans / 1000
     crates = jars / 100
-    return jelly beans, jars, crates
+    return jelly_beans, jars, crates
 
 start_point = 10000
 beans, jars, crates = secret_formula(start_point)
